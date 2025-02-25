3c03f27f3f59e68192f8ca45c919a9ea3cf6d366
Marcos Antônio Ribeiro Silva Norte <marcosantoniosilvanorte@gmail.com>
2025-02-25 11:00:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var imageUrl = 'sua-imagem.png';
+
 
 document.getElementById('searchForm').addEventListener('submit', function(e) {
     e.preventDefault();
@@ -7,72 +7,29 @@ document.getElementById('searchForm').addEventListener('submit', function(e) {
     resultDiv.innerHTML = '';
 
     
+    if (!navigator.onLine) {
+        startFlappyBirdGame();
+        return;  
+    }
+
     var urlPattern = /^(https?:\/\/)?([\w.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/i;
     var completeUrlPattern = /^(https?:\/\/)/i;
 
-    
     var youtubePattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
 
     if (urlPattern.test(query)) {
-        
         if (!completeUrlPattern.test(query)) {
-            query = 'http://' + query;
+            query = 'https://' + query;  
         }
 
         
-        var img = document.createElement('img');
-        img.src = 'https://www.google.com/s2/favicons?domain=' + new URL(query).hostname;
-        resultDiv.appendChild(img);
+        if (completeUrlPattern.test(query)) {
+            var img = document.createElement('img');
+            img.src = 'https://www.google.com/s2/favicons?domain=' + new URL(query).hostname;
+            resultDiv.appendChild(img);
 
-        if (youtubePattern.test(query)) {
-           
-            var videoId = query.split('v=')[1] || query.split('/').pop();
-            var ampersandPosition = videoId.indexOf('&');
-            if (ampersandPosition !== -1) {
-                videoId = videoId.substring(0, ampersandPosition);
-            }
-
-            
-            var thumbnailUrl = 'https://img.youtube.com/vi/' + videoId + '/0.jpg';
-            var apiUrl = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + videoId + '&key=AIzaSyAH121LnYi033eATvjq5HLf4t6cHMpid14';
-
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    var videoTitle = data.items[0].snippet.title;
-
-                    var redirectText = document.createElement('span');
-                    redirectText.textContent = 'Redirecionando para YouTube';
-                    resultDiv.appendChild(redirectText);
-
-                    var videoContainer = document.createElement('div');
-                    videoContainer.style.border = '1px solid black';
-                    videoContainer.style.padding = '10px';
-                    videoContainer.style.marginTop = '10px';
-                    videoContainer.style.display = 'inline-block';
-
-                    var img = document.createElement('img');
-                    img.src = thumbnailUrl;
-                    img.style.maxWidth = '150px';
-                    img.style.marginRight = '10px';
-                    videoContainer.appendChild(img);
-
-                    var text = document.createElement('span');
-                    text.textContent = videoTitle;
-                    videoContainer.appendChild(text);
-
-                    resultDiv.appendChild(videoContainer);
-
-                    setTimeout(function() {
-                        window.location.href = query;
-                    }, 2000); 
-                });
-        } else {
             var text = document.createElement('span');
-            
-           
-            var siteName = new URL(query).hostname.replace('www.', '');
-            text.textContent = 'Redirecionando para ' + siteName;
+            text.textContent = 'Redirecionando para ' + new URL(query).hostname;
             resultDiv.appendChild(text);
 
             setTimeout(function() {
@@ -85,3 +42,30 @@ document.getElementById('searchForm').addEventListener('submit', function(e) {
         window.location.href = searchUrl;
     }
 });
+
+
+function startFlappyBirdGame() {
+    document.getElementById('gameContainer').style.display = 'block';
+    var bird = document.getElementById('bird');
+    var birdY = 200;
+    var gravity = 2;
+    var lift = -30;
+    var velocity = 0;
+
+   
+    document.addEventListener('keydown', function() {
+        velocity = lift;  
+    });
+
+    function gameLoop() {
+        velocity += gravity;
+        birdY += velocity;
+        if (birdY < 0) birdY = 0;  
+        if (birdY > 370) birdY = 370;  
+        bird.style.top = birdY + 'px';
+
+        requestAnimationFrame(gameLoop);
+    }
+
+    gameLoop();  
+}
